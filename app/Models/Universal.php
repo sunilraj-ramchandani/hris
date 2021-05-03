9cc925f1de2eb500a7812e6082e64abc6df07087
sunilraj-globe <80705492+sunilraj-globe@users.noreply.github.com>
2021-05-03 08:58:39 -0700

    added company filter
    company filter added to select table

--- app/Models/Universal.php
+++ app/Models/Universal.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\CustomValue;
 use Illuminate\Support\Facades\DB;
 use Yajra\DataTables\Facades\DataTables;
+use Session;
 
 class Universal extends Model
 {
     public static function selectTable($table){
-        $select = 'select * from hris.'.$table . ' where deleted_at is null';
+        $select = 'select * from hris.'.$table . ' where deleted_at is null and company_id ='.Session::get('company');
         $var_table = DB::select($select);
         return $var_table;
     }
